15b143529360e29e309d711923197118364a2655
furkantokinsider <furkan.tok@useinsider.com>
2024-08-25 23:00:18 +0300

    initial commit

--- utils/enums.py
+++ utils/enums.py
@@ -1,1 +1,14 @@
 test_partner = "ucdtest_13"
+UCD_ENDPOINT = "http://atrium.insidethekube.com/api"
+opt_taskId = "taskId"
+
+HEADERS = {
+    "Content-Type": "application/json",
+    "x-ins-namespace": opt_taskId
+}
+
+
+class ENDPOINTS:
+    ATTRIBUTE_V2_UPDATE = "/attribute/v2/update"
+    CONTACT_API = "/contact/v1/profile"
+    VERTICAL_API = "/vertical/v1/profile"
